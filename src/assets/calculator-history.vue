f303ee37e0655d838fadb0fc9e13b198dbdbefe9
AndreyBallakh <andrey.ballakh@gmail.com>
2023-03-14 21:31:27 +0200

    add history

---
+++ src/assets/calculator-history.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="history">
+    <span>{{ greetingMessage }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CalculatorHistory',
+    props: {
+        greetingMessage: String
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+    .history {
+        background-color: grey;
+        color: #fff;
+        ul {
+            list-style: none;
+        }
+    }
+</style>
